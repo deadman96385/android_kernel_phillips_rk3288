0b21994f312048b49dcbe0ee987001fc601b1af8
deadman96385 <seanhoyt963@gmail.com>
2026-03-11 18:34:54 -0500

    feat: input: enhance Rockchip PWM remote control with TPV RC6 support
    - Extend Rockchip PWM remote control driver with TPV RC6 protocol support.
    - New component:
    - rockchip_pwm_remotectl_tpv_rc6.c: TPV RC6 protocol implementation (225 lines)
    - Enhancements:
    - Add protocol-specific button lookup logic

--- drivers/input/remotectl/rockchip_pwm_remotectl.h
+++ drivers/input/remotectl/rockchip_pwm_remotectl.h
@@ -91,7 +91,7 @@ enum pwm_div {
 
 
 /********************************************************************
-**                            宏定义                                *
+**                            锟疥定锟斤拷                                *
 ********************************************************************/
 #define RK_PWM_TIME_PRE_MIN	4000
 #define RK_PWM_TIME_PRE_MAX	5000
@@ -116,9 +116,10 @@ enum pwm_div {
 #define PWM_CH_INT(n)   BIT(n)
 #define PWM_CH_POL(n)   BIT(n+8)
 
+//#define CHECK_PWM_REGISTERS_PER_1S
 
 /********************************************************************
-**                          结构定义                                *
+**                          锟结构锟斤拷锟斤拷                                *
 ********************************************************************/
 typedef enum _RMC_STATE
 {
@@ -138,5 +139,190 @@ struct RKxx_remotectl_platform_data {
 	int wakeup;
 };
 
+typedef enum _RMC_PROTOCOL_ING
+{
+    RMC_PROTOCOL_NOTHING    = 0xff,
+    RMC_PROTOCOL_NEC    = 0x10,
+    RMC_PROTOCOL_RC6    = 0x20,
+
+}eRMC_PROTOCOL_ING;
+
+#define RMC_START_MIN           50000
+#define RMC_NEC_STARTER_MIN     8000
+#define RMC_NEC_STARTER_MAX     10000
+#define RMC_RC6_STARTER_MIN     2500
+#define RMC_RC6_STARTER_MAX     3000
+
+
+#define RC6_BIT_COUNT 52
+#define RC6_RAW_LS_1        0b1100
+#define RC6_RAW_LS_2    0b1111
+#define RC6_RAW_SB      0b10
+#define RC6_RAW_LS_2    0b1111
+#define RC6_RAW_LS_2    0b1111
+#define RC6_RAW_LS_2    0b1111
+
+
+#define RAW_LS_H_4BITS        0b1111
+#define RAW_LS_L_4BITS        0b1100
+#define RAW_SB_BITS           0b10
+#define RAW_MODE2_BITS        0b01
+#define RAW_MODE1_BITS        0b01
+#define RAW_MODE0_BITS        0b01
+
+#define RAW_TR_0_BITS        0b0011
+#define RAW_TR_1_BITS        0b1100
+
+#define RAW_BIT0             0b01
+#define RAW_BIT1             0b10
+
+#define RAW_RC6_L_MEAN          450
+#define RAW_RC6_H_MEAN          420
+
+#define RAW_RC6_HIGH_POLARITY    0 
+
+struct tpv_ir_rc6_raw {
+	unsigned raw_info       :16;
+	unsigned raw_ctrl       :16;
+
+	unsigned raw_tr         :4; //1100 or 0011
+	unsigned raw_mode_0     :2; //01
+	unsigned raw_mode_1     :2; //01
+
+	unsigned raw_mode_2     :2; //01
+	unsigned raw_sb         :2; //10
+	unsigned raw_ls_1       :4; //1100
+	unsigned raw_ls_2       :4; //1111
+};
+
+union Rc6Raw_U
+{
+    struct tpv_ir_rc6_raw rawdata;
+    unsigned long long bits;
+};
+
+ 
+struct tpv_ir_rc6_data {
+	unsigned dec_ls;
+	unsigned dec_sb;
+	unsigned dec_mode;
+	unsigned dec_tr;
+	unsigned dec_ctrl;
+	unsigned dec_info;
+};
+
+
+typedef enum _RMC_RC6_RET
+{
+    RC6_GOOD,
+    RC6_ERR_LS          = -1,
+    RC6_ERR_SB          = -2,
+    RC6_ERR_MODE        = -3,
+    RC6_ERR_TR          = -4,
+    RC6_ERR_CTRL        = -5,
+    RC6_ERR_INFO        = -6,
+    RC6_ERR_LENGTH      = -7,
+    RC6_ERR_DATA      = 0xffff,
+}eRMC_RC6_RET;
+
+struct rkxx_remote_key_table {
+	int scancode;
+	int keycode;
+};
+
+struct rkxx_remotectl_button {
+	int procotol;
+	int usercode;
+	int nbuttons;
+	struct rkxx_remote_key_table key_table[MAX_NUM_KEYS];
+};
+
+struct tpv_rc6_data {
+	union Rc6Raw_U raw;
+	struct tpv_ir_rc6_data keydata;
+	int bit_count;
+	int keynum;
+	int keycode_ev;
+	int press;
+	struct timer_list rc_timer;
+};
+struct rkxx_remotectl_drvdata {
+	void __iomem *base;
+	int state;
+	int nbuttons;
+	int result;
+	int scandata;
+	int count;
+	int keynum;
+	int maxkeybdnum;
+	int keycode;
+	int press;
+	int pre_press;
+	int irq;
+	int remote_pwm_id;
+	int handle_cpu_id;
+	int wakeup;
+	int clk_rate;
+	int support_psci;
+	unsigned long period;
+	unsigned long temp_period;
+	int pwm_freq_nstime;
+	struct input_dev *input;
+	struct timer_list timer;
+	struct timer_list dbg_timer;
+	struct tasklet_struct remote_tasklet;
+	struct wake_lock remotectl_wake_lock;
+	int protocol_state;
+    struct tpv_rc6_data rc6;
+};
+
+
+extern int rk_remote_print_code;
+#define DBG_CODE(fmt, args...) \
+	do { \
+		if (rk_remote_print_code) { \
+			pr_info("MDC-DBG_CODE,M:%s,L:%d. "fmt,__FUNCTION__,__LINE__, ##args); \
+		} \
+	} while (0)
+
+extern int rk_remote_pwm_dbg_level;
+#define DBG(fmt, args...) \
+	do { \
+		if (rk_remote_pwm_dbg_level) { \
+			pr_info("MDC-DBG,M:%s,L:%d. "fmt,__FUNCTION__,__LINE__, ##args); \
+		} \
+	} while (0)
+
+extern int rk_remote_pwm_dbg_rc6;
+#define DBG_RC6(fmt, args...) \
+	do { \
+		if (rk_remote_pwm_dbg_rc6) { \
+			pr_info("MDC-RC6,M:%s,L:%d. "fmt,__FUNCTION__,__LINE__, ##args); \
+		} \
+	} while (0)
+
+
+extern struct rkxx_remotectl_button *remotectl_button;
+
+
+void tpv_rc6_remotectl_timer(unsigned long _data);
+int tpv_rc6_remotectl_keybd_num_lookup(struct rkxx_remotectl_drvdata *ddata);
+int tpv_rc6_remotectl_keycode_lookup(struct rkxx_remotectl_drvdata *ddata);
+void tpv_rc6_raw_fill_bits(struct rkxx_remotectl_drvdata *ddata, int level, int number);
+int tpv_rc6_raw16_to_code(int in);
+void tpv_rc6_dump_keycode(struct rkxx_remotectl_drvdata *ddata);
+int tpv_rc6_decode(struct rkxx_remotectl_drvdata *ddata);
+void tpv_rc6_reset(struct rkxx_remotectl_drvdata *ddata);
+void tpv_rc6_handler(struct rkxx_remotectl_drvdata *ddata, unsigned h_time, unsigned l_time);
+
+static inline int isHighStarter(unsigned h){
+    return h > RMC_START_MIN;
+}
+static inline int isRC6LowStarter(unsigned l){
+    return l >RMC_RC6_STARTER_MIN && l < RMC_RC6_STARTER_MAX;
+}
+static inline int isNECLowStarter(unsigned l){
+    return l >RMC_NEC_STARTER_MIN && l < RMC_NEC_STARTER_MAX;
+}
 #endif
 
